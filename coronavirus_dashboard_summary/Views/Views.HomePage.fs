7124177075095e5a60a4c98a9a07a05f81145bd9
Pouria Hadjibagheri <p.h.bagheri@gmail.com>
2021-10-28 12:33:12 +0100

    integrates generic banners + housekeeping

--- coronavirus_dashboard_summary/Views/Views.HomePage.fs
+++ coronavirus_dashboard_summary/Views/Views.HomePage.fs
@@ -1,7 +1,5 @@
 module coronavirus_dashboard_summary.Views.HomePageView 
 
-open System
-open Giraffe.ViewEngine
 open Giraffe
 open Microsoft.AspNetCore.Http
 open FSharp.Control.Tasks
@@ -11,126 +9,43 @@ open coronavirus_dashboard_summary.Templates.Base
 open coronavirus_dashboard_summary.Templates
 open coronavirus_dashboard_summary.Utils    
 open coronavirus_dashboard_summary.Utils.TimeStamp
-open System.Collections
-
-let HomePageMetrics =
-    [|
-        "newAdmissions"
-        "newAdmissionsChange"
-        "newAdmissionsChangePercentage"
-        "newAdmissionsRollingSum"
-        "newAdmissionsDirection"
-
-        "newPeopleVaccinatedFirstDoseByPublishDate"
-        "newPeopleVaccinatedSecondDoseByPublishDate"
-        "cumPeopleVaccinatedFirstDoseByPublishDate"
-        "cumPeopleVaccinatedSecondDoseByPublishDate"
-        "cumVaccinationFirstDoseUptakeByPublishDatePercentage"
-        "cumVaccinationSecondDoseUptakeByPublishDatePercentage"
-
-        "newDeaths28DaysByPublishDate"
-        "newDeaths28DaysByPublishDateChange"
-        "newDeaths28DaysByPublishDateChangePercentage"
-        "newDeaths28DaysByPublishDateRollingSum"
-        "newDeaths28DaysByPublishDateDirection"
-        "newDeaths28DaysByDeathDateRollingRate"
-
-        "newCasesByPublishDate"
-        "newCasesByPublishDateChange"
-        "newCasesByPublishDateChangePercentage"
-        "newCasesByPublishDateRollingSum"
-        "newCasesByPublishDateDirection"
-        "newCasesBySpecimenDateRollingRate"
-
-        "newVirusTests"
-        "newVirusTestsChange"
-        "newVirusTestsChangePercentage"
-        "newVirusTestsRollingSum"
-        "newVirusTestsDirection"
-
-    |]
-
-let inline formatDate (d: DateTime) =
-    d.ToString "d MMMM yyyy"
     
-let inline subtractFormatDate (d: DateTime) (n: float) =
-    d.AddDays n
-    |> formatDate
-    
-let leadSection =
-    [
-        div [] [
-            div [ _class "govuk-grid-row" ] [
-                div [ _class "govuk-grid-column-full" ] [
-                    h1 [ _class "govuk-heading-l govuk-!-margin-bottom-2 govuk-!-margin-top-2"
-                         _data "nosnippet" "true" ] [ encodedText "UK Summary" ]
-                    p [ _class "govuk-body-m govuk-!-margin-bottom-1 govuk-!-margin-top-3" ] [
-                        encodedText
-                            "The official UK government website for data and insights on coronavirus (COVID-19)."
-                    ]
-                ]
-            ]
-        ]
-        div [] [
-            p [ _class "govuk-body-m govuk-!-margin-bottom-3 govuk-!-margin-top-2" ] [
-                encodedText "See the "
-                a [ _class "govuk-link govuk-link--no-visited-state"; _href "/easy_read" ] [
-                    encodedText "simple summary"
-                ]
-                encodedText " for the UK."
-            ]
-        ]
-    ]
-    
-let inline changeLogBanners redis date =
-    ChangeLogModel.ChangeLog.Data redis date
-    |> Async.RunSynchronously
-    |> ChangeLogBanners.Render
-    
-let inline private filterPayload (group: string * DB.Payload list): DB.Payload =
-    snd group
-    |> Seq.minBy (fun v -> v.priority)
-    
-let index (date: Release) (redis: Redis.Client): XmlNode List =    
+let index (date: Release) (redis: Redis.Client) =    
     let dbResp =
         redis.GetAllAsync [|$"area-{date.isoDate}-UK"|]
         |> Async.RunSynchronously
         |> Json.deserialize<DB.Payload list>
         |> List.groupBy (fun item -> item.metric)
-        |> List.map filterPayload
+        |> List.map Filters.ByPriorityAttribute
         |> List.map (fun item -> (item.metric, item))
         |> dict
         |> Metrics.GeneralPayload
         
     [
-        yield! leadSection
-        article [] [
-            ul [ _class "card-container" ] [
-                yield!
-                    MetaData.CardMetadata
-                    |> Array.Parallel.map (fun metadata -> metadata.Card date dbResp null)
-                    |> List.concat
-            ]
-        ]
+        yield! HomeHeading.Render
+                    
+        MetaData.CardMetadata
+        |> Array.Parallel.map (fun metadata -> metadata.Card date dbResp null)
+        |> List.concat
+        |> Body.Render
     ]
 
 let HomePageHandler =
     fun (next: HttpFunc) (ctx: HttpContext) ->
         task {
-            let date = ReleaseTimestamp()
+            let release = ReleaseTimestamp()
             let redis = ctx.GetService<Redis.Client>()
-            let changeLogs = changeLogBanners redis date
             
             let layout: LayoutPayload =
                 {
-                    date          = date
-                    changeLogs    = changeLogs
-                    title         = "UK Summary"
-                    postcode      = null
+                    date     = release
+                    banners  = Banners.Render redis release
+                    title    = "UK Summary"
+                    postcode = null
                 }
 
             return!
-                index date redis
+                index release redis
                 |> layout.Render
                 |> ctx.WriteHtmlViewAsync
         }
--- coronavirus_dashboard_summary/Views/Views.PostCode.fs
+++ coronavirus_dashboard_summary/Views/Views.PostCode.fs
@@ -1,153 +1,71 @@
 module coronavirus_dashboard_summary.Views.PostCodeSearch
 
-open System.Text.RegularExpressions
 open Microsoft.AspNetCore.Http
 open FSharp.Control.Tasks
 open Giraffe
-open Giraffe.ViewEngine
 open FSharp.Json
-open coronavirus_dashboard_summary.Models.ChangeLogModel
 open Microsoft.AspNetCore.ResponseCaching
-open coronavirus_dashboard_summary.Models.AnnouncementModel
 open coronavirus_dashboard_summary.Templates
 open coronavirus_dashboard_summary.Templates.Base
 open coronavirus_dashboard_summary.Models
 open coronavirus_dashboard_summary.Models.MetaData
 open coronavirus_dashboard_summary.Utils
-open coronavirus_dashboard_summary.Views.HomePageView
 open coronavirus_dashboard_summary.Utils.TimeStamp
-open System.Collections
-
-let PostCodeMetrics =
-    [|
-        "newAdmissions"
-        "newAdmissionsChange"
-        "newAdmissionsChangePercentage"
-        "newAdmissionsRollingSum"
-        "newAdmissionsDirection"
-        "newPeopleVaccinatedFirstDoseByPublishDate"
-        "newPeopleVaccinatedSecondDoseByPublishDate"
-        "cumPeopleVaccinatedFirstDoseByPublishDate"
-        "cumPeopleVaccinatedSecondDoseByPublishDate"
-        "cumVaccinationFirstDoseUptakeByPublishDatePercentage"
-        "cumVaccinationSecondDoseUptakeByPublishDatePercentage"
-        "cumVaccinationFirstDoseUptakeByVaccinationDatePercentage"
-        "cumVaccinationSecondDoseUptakeByVaccinationDatePercentage"
-        "newDeaths28DaysByPublishDate"
-        "newDeaths28DaysByPublishDateChange"
-        "newDeaths28DaysByPublishDateChangePercentage"
-        "newDeaths28DaysByPublishDateRollingSum"
-        "newDeaths28DaysByPublishDateDirection"
-        "newDeaths28DaysByDeathDateRollingRate"
-        "newCasesBySpecimenDateRollingSum"
-        "newCasesBySpecimenDateRollingRate"
-        "newCasesBySpecimenDate"
-        "newCasesByPublishDate"
-        "newCasesByPublishDateChange"
-        "newCasesByPublishDateChangePercentage"
-        "newCasesByPublishDateRollingSum"
-        "newCasesByPublishDateDirection"
-        "newVirusTests"
-        "newVirusTestsChange"
-        "newVirusTestsChangePercentage"
-        "newVirusTestsRollingSum"
-        "newVirusTestsDirection"
-        "transmissionRateMin"
-        "transmissionRateMax"
-    |]
-
-let inline private (|Regex|_|) pattern input =
-    let found = Regex.Match(input, pattern)
-    match found.Success with
-    | true -> Some(List.tail [ for g in found.Groups -> g.Value ])
-    | false -> None
-
-let inline private validatePostcode (postcode: string): string =
-    match postcode.ToUpper() with
-    | Regex @"^\s*([A-Z]{1,2}\d{1,2}[A-Z]?\s?\d{1,2}[A-Z]{1,2})\s*$" [ validated ] ->
-        validated.Replace(" ", "").ToUpper()
-    | _ -> ""
-
-let inline private PageHeading (postcode: string) =
-    div [ _id "top" ] [
-        div [ _class "sticky-header govuk-!-padding-top-3" ] [
-            div [ _class "sticky-header govuk-grid-row govuk-!-margin-top-0" ] [
-                div [ _class "govuk-grid-column-one-half" ] [
-                    h1 [ _class "govuk-heading-l govuk-!-margin-bottom-2 govuk-!-margin-top-0" ] [
-                        encodedText $"Local summary for { postcode.ToUpper() }"
-                    ]
-                ]
-            ]
-        ]
-    ]
-    
-let inline private filterPayload (group: string * DB.Payload list) =
-    snd group
-    |> Seq.minBy (fun v -> v.priority)
+open coronavirus_dashboard_summary.Views.HomePageView
     
 type private PostCodeView(postcode: string, redis: Redis.Client) =
     
-    let date = ReleaseTimestamp()
+    let release = ReleaseTimestamp()
             
     let postcodeAreas =
-        PostCode.Model(redis, date, validatePostcode postcode).PostCodeAreas
+        PostCode.Model(redis, release, Validators.ValidatePostcode postcode).PostCodeAreas
         |> Async.RunSynchronously
         
-    member inline private _.changeLogBanners() =
-        ChangeLog.Data redis date
-        |> Async.RunSynchronously
-        |> ChangeLogBanners.Render
-            
-    member inline private _.announcementBanners() =
-        div[] []            
     member _.postCodeFound
-        with get() = not <| List.isEmpty postcodeAreas
+        with get() = List.isEmpty postcodeAreas
+                     |> not
+        
+    member private _.getContent (postcodeData: DB.PostCodeDataPayload list) = 
+        let dbResp =
+            postcodeData
+            |> List.map (fun item -> item.Key release redis)
+            |> List.toArray
+            |> redis.GetAllAsync
+            |> Async.RunSynchronously
+            |> Json.deserialize<DB.Payload List>
+            |> List.groupBy (fun item -> item.metric)
+            |> List.map Filters.ByPriorityAttribute
+            |> List.map (fun item -> (item.metric, item))
+            |> dict
+            |> Metrics.GeneralPayload
+
+        [
+            PostCodeHeading.Render postcode
+            
+            CardMetadata
+            |> Array.Parallel.map (fun metadata -> metadata.Card release dbResp postcode)
+            |> List.concat
+            |> Body.Render
+        ]
         
     member this.postcodeData =        
         postcodeAreas
         |> (fun postcodeData ->
                 let layout: LayoutPayload =
                     {
-                        date          = date
-                        changeLogs    = this.changeLogBanners()
-                        title         = $"Local summary for { postcode.ToUpper() }"
-                        postcode      = match postcodeData.IsEmpty with
-                                        | true -> postcode
-                                        | _ -> null
+                        date     = release
+                        title    = $"Local summary for { postcode.ToUpper() }"
+                        banners  = Banners.Render redis release
+                        postcode = match postcodeData.IsEmpty with
+                                   | true -> postcode
+                                   | _ -> null
                     }
                 
-                match postcodeData with
-                | [] -> index date redis
-                | _ -> postcodeData |> this.getContent
+                match postcodeData.IsEmpty with
+                | true  -> index release redis
+                | false -> postcodeData |> this.getContent
                 |> layout.Render
         )
-        
-    member private _.getContent (postcodeData: DB.PostCodeDataPayload list) = 
-        let dbResp =
-            postcodeData
-            |> List.map (fun item -> item.Key date PostCodeMetrics redis)
-            |> List.toArray
-            |> redis.GetAllAsync
-            |> Async.RunSynchronously
-            |> Json.deserialize<DB.Payload List>
-            |> List.groupBy (fun item -> item.metric)
-            |> List.map filterPayload
-            |> List.map (fun item -> (item.metric, item))
-            |> dict
-            |> Metrics.GeneralPayload
-
-        [
-            PageHeading postcode
-            
-            article [] [                
-                ul [ _class "govuk-list card-container" ] [
-                    yield!
-                        CardMetadata
-                        |> Array.Parallel.map (fun metadata -> metadata.Card date dbResp postcode)
-                        |> List.concat
-                ]
-            ]
-        ]
 
 let PostCodePageHandler: HttpHandler =
     fun (next: HttpFunc) (ctx: HttpContext) ->
@@ -156,7 +74,7 @@ let PostCodePageHandler: HttpHandler =
 
             match responseCachingFeature with
             | null -> null |> ignore
-            | _ -> responseCachingFeature.VaryByQueryKeys <- [| "postcode" |]
+            | _    -> responseCachingFeature.VaryByQueryKeys <- [| "postcode" |]
             
             let postcode =
                 ctx.TryGetQueryStringValue "postcode"
@@ -166,7 +84,7 @@ let PostCodePageHandler: HttpHandler =
                 ctx.GetService<Redis.Client>()
                 
             let view =
-                PostCodeView(postcode, redis)
+                PostCodeView(postcode.ToUpper(), redis)
             
             if not view.postCodeFound
                 then ctx.SetStatusCode 404
