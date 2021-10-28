584969b34ac3994440820310543a2772b697e714
Pouria Hadjibagheri <p.h.bagheri@gmail.com>
2021-10-28 10:08:39 +0100

    simplifies implementation of Processor

--- coronavirus_dashboard_summary/Utils/Utils.Metrics.fs
+++ coronavirus_dashboard_summary/Utils/Utils.Metrics.fs
@@ -4,12 +4,13 @@ open System.Collections.Generic
 open coronavirus_dashboard_summary.Models
 open coronavirus_dashboard_summary.Utils.Constants
 
-type MetricValue (payload: Dictionary<string, DB.Payload>) =
-    member _.metricValue =
-        let inline metric (metric: string) =
-            let inline attr (attribute: string) =
-                match payload.TryGetValue metric with
-                | true, m -> m.getter attribute
-                | _ -> Generic.NotAvailable
-            attr
-        metric
+type GeneralPayload = Dictionary<string, DB.Payload>
+
+let inline Processor (payload: GeneralPayload) =
+    let inline metric (metric: string) =
+        let inline attr (attribute: string) =
+            match payload.TryGetValue metric with
+            | true, m -> m.getter attribute
+            | _ -> Generic.NotAvailable
+        attr
+    metric
