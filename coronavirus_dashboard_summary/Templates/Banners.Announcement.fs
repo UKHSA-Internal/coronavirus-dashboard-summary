1a96859d35465c70ed8e08f5dcd8f3fb54380c77
Pouria Hadjibagheri <p.h.bagheri@gmail.com>
2021-10-28 12:24:36 +0100

    Adds banner announcements renderer

---
+++ coronavirus_dashboard_summary/Templates/Banners.Announcement.fs
@@ -0,0 +1,35 @@
+module coronavirus_dashboard_summary.Templates.Announcement
+
+open Giraffe.ViewEngine
+open FSharp.Formatting.Markdown
+open coronavirus_dashboard_summary.Models
+open coronavirus_dashboard_summary.Utils
+open coronavirus_dashboard_summary.Utils.Constants
+
+let inline private bannerItem (banner: DB.AnnouncementPayload) =
+    li [ _class "content" ] [
+        strong [ _class "timestamp" ] [
+            time [
+                banner.date
+                |> Formatter.toIsoString
+                |> _datetime
+            ] [
+                banner.date.ToString Generic.DateFormat
+                |> encodedText
+            ]
+        ]
+        div [ _class "body" ] [
+            banner.body
+            |> Markdown.ToHtml
+            |> rawText
+        ]
+    ]  
+
+let inline Render (payload: DB.AnnouncementPayload list): XmlNode =
+    match payload.IsEmpty with
+    | true  -> "" |> rawText
+    | false -> ul [ _class "banner" ] [
+                   yield! 
+                       payload
+                       |> List.map bannerItem
+               ]
