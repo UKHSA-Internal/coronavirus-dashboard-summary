87f94cbb949e1577ccb7e051753107ed7deafd95
Pouria Hadjibagheri <p.h.bagheri@gmail.com>
2021-10-28 12:31:12 +0100

    adds home page heading template

---
+++ coronavirus_dashboard_summary/Templates/Home.Heading.fs
@@ -0,0 +1,28 @@
+module coronavirus_dashboard_summary.Templates.HomeHeading
+
+open Giraffe.ViewEngine
+
+let Render =
+    [
+        div [] [
+            div [ _class "govuk-grid-row" ] [
+                div [ _class "govuk-grid-column-full" ] [
+                    h1 [ _class "govuk-heading-l govuk-!-margin-bottom-2 govuk-!-margin-top-2"
+                         _data "nosnippet" "true" ] [ encodedText "UK Summary" ]
+                    p [ _class "govuk-body-m govuk-!-margin-bottom-1 govuk-!-margin-top-3" ] [
+                        encodedText
+                            "The official UK government website for data and insights on coronavirus (COVID-19)."
+                    ]
+                ]
+            ]
+        ]
+        div [] [
+            p [ _class "govuk-body-m govuk-!-margin-bottom-3 govuk-!-margin-top-2" ] [
+                encodedText "See the "
+                a [ _class "govuk-link govuk-link--no-visited-state"; _href "/easy_read" ] [
+                    encodedText "simple summary"
+                ]
+                encodedText " for the UK."
+            ]
+        ]
+    ]
