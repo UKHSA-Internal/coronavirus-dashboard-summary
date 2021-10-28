526c9033ead1b5db3f32a9d2e9d2ac46f768ae34
Pouria Hadjibagheri <p.h.bagheri@gmail.com>
2021-10-28 09:55:50 +0100

    simplifies the implementation

--- coronavirus_dashboard_summary/Utils/Utils.Metrics.fs
+++ coronavirus_dashboard_summary/Utils/Utils.Metrics.fs
@@ -1,47 +1,15 @@
 module coronavirus_dashboard_summary.Utils.Metrics
 
+open System.Collections.Generic
 open coronavirus_dashboard_summary.Models
 open coronavirus_dashboard_summary.Utils.Constants
 
-type MetricValue (payload: DB.Payload list) =
-    member inline private _.getData (data: DB.Payload list) =
-        fun metric ->
-            let metricData =
-                data
-                |> List.tryFind (fun elm -> elm.metric.Equals metric)
-                
-            fun attribute ->
-                match metricData with
-                | Some v -> v |> fun elm -> elm.getter attribute
-                | None -> Generic.NotAvailable
-        
-    member inline private _.filterPayload (group: string * DB.Payload List) =
-        snd group
-        |> Seq.minBy (fun v -> v.priority)
-        
-    member this.msoaMetricValue =
-        let resp = payload
-                   |> List.filter (fun elm -> elm.area_type.Equals AreaTypes.MSOA)
-                   
-        match resp.IsEmpty with
-        | true -> payload |> this.getData
-        | false -> resp |> this.getData
-        
-    member _.smallestByMetric metric attribute =
-        let res =
-            payload
-            |> List.filter (fun elm -> elm.metric = metric)
-        
-        match List.isEmpty res with
-        | true -> Generic.NotAvailable
-        | _ -> res
-               |> List.minBy (fun elm -> elm.priority)
-               |> (fun elm -> elm.getter attribute)
-              
-        
-    member this.metricValue  =
-        payload
-        |> List.filter (fun elm -> elm.area_type.Equals AreaTypes.MSOA |> not)
-        |> List.groupBy (fun item -> item.metric)
-        |> List.map this.filterPayload
-        |> this.getData
+type MetricValue (payload: Dictionary<string, DB.Payload>) =
+    member _.metricValue =
+        let inline metric (metric: string) =
+            let inline attr (attribute: string) =
+                match payload.TryGetValue metric with
+                | true, m -> m.getter attribute
+                | _ -> Generic.NotAvailable
+            attr
+        metric
