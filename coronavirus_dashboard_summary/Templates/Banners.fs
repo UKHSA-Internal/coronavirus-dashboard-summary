9f35e79d80034d6dd6bd427a36c27da90e684694
Pouria Hadjibagheri <p.h.bagheri@gmail.com>
2021-10-28 12:27:36 +0100

    adds generic banner renderer

---
+++ coronavirus_dashboard_summary/Templates/Banners.fs
@@ -0,0 +1,29 @@
+module coronavirus_dashboard_summary.Templates.Banners
+
+open Giraffe.ViewEngine
+open coronavirus_dashboard_summary.Models
+open coronavirus_dashboard_summary.Templates
+open coronavirus_dashboard_summary.Utils
+
+type BannerPayload =
+    {
+        changeLogs:    XmlNode option
+        announcements: XmlNode
+    }
+
+let inline Render (redis: Redis.Client) (release: TimeStamp.Release): BannerPayload =
+    async {
+        let! changeLog =
+            ChangeLogModel.ChangeLog.Data redis release
+            
+        let! announcement =
+            AnnouncementModel.Announcements.Data redis release
+        
+        return
+            {
+                changeLogs    = changeLog    |> ChangeLogBanners.Render
+                announcements = announcement |> Announcement.Render
+            }
+    }
+    |> Async.RunSynchronously
+
