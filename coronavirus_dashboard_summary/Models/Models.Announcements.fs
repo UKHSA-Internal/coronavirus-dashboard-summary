b633bbe5e7e0676c25ca8c11c4412772e806dd4d
Pouria Hadjibagheri <p.h.bagheri@gmail.com>
2021-10-28 09:56:08 +0100

    updates query

--- coronavirus_dashboard_summary/Models/Models.Announcements.fs
+++ coronavirus_dashboard_summary/Models/Models.Announcements.fs
@@ -23,34 +23,26 @@ type Announcements(redis, date) =
        
     override this.query =
         """WITH latest_release AS (
-            SELECT MAX(rr.timestamp)::DATE
-            FROM covid19.release_reference AS rr
-            WHERE rr.released IS TRUE
-        )
-        SELECT COALESCE(date, launch::DATE) AS date, body
-        FROM covid19.announcement AS an
-        WHERE
-            (
-                (
-                        an.deploy_with_release IS TRUE
-                    AND an.launch::DATE <= (SELECT * FROM latest_release)
-                )
-              OR (
-                        an.deploy_with_release IS FALSE
-                    AND an.launch <= NOW()
-                )
-            )
-          AND (
-                (
-                        an.remove_with_release IS TRUE
-                    AND an.expire::DATE > (SELECT * FROM latest_release)
-                )
-              OR (
-                        an.remove_with_release IS FALSE
-                    AND an.expire > NOW()
-                )
-            )
-        ORDER BY an.launch DESC, an.expire DESC;"""
+    SELECT MAX(rr.timestamp)::DATE
+    FROM covid19.release_reference AS rr
+    WHERE rr.released IS TRUE
+)
+SELECT id::TEXT,
+       launch::DATE,
+       expire::DATE,
+       COALESCE(date, launch::DATE) AS date,
+       body
+FROM covid19.announcement AS an
+WHERE
+    (
+        ( an.deploy_with_release IS TRUE AND an.launch::DATE <= (SELECT * FROM latest_release) )
+      OR ( an.deploy_with_release IS FALSE AND an.launch <= NOW() )
+    )
+  AND (
+        ( an.remove_with_release IS TRUE AND an.expire::DATE > (SELECT * FROM latest_release) )
+      OR ( an.remove_with_release IS FALSE AND an.expire > NOW() )
+    )
+ORDER BY an.launch DESC, an.expire DESC;"""
       
     static member inline Data redis date: Async<AnnouncementPayload list> =
         let fetcher = Announcements(redis, date)
