a7c099794f17020c4299a5823aca59198f59d096
Pouria Hadjibagheri <p.h.bagheri@gmail.com>
2021-11-28 14:24:27 +0000

    integrates expiry date into banner query

--- coronavirus_dashboard_summary/Models/Models.ChangeLog.fs
+++ coronavirus_dashboard_summary/Models/Models.ChangeLog.fs
@@ -18,7 +18,11 @@ FROM covid19.change_log AS cl
   LEFT JOIN covid19.change_log_to_page   AS cltp ON cltp.log_id = cl.id
   LEFT JOIN covid19.page                 AS p ON p.id = cltp.page_id
 WHERE cl.display_banner IS TRUE
-  AND date = @date::DATE
+  AND (
+        ( date = @date::DATE AND expiry ISNULL )
+     OR ( date <= @date::DATE AND expiry >= NOW()::DATE )
+  )
+  AND (
   AND (
  	   cl.area ISNULL
     OR cl.area @> '{overview::^K.*$}'::VARCHAR[]
