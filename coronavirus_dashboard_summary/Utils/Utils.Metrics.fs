acc58dc859717298424e509cdc1eaae6e08109c1
Pouria Hadjibagheri <p.h.bagheri@gmail.com>
2022-02-27 12:30:07 +0000

    Implements conversion to dict into GeneralPayload + adds docs

--- coronavirus_dashboard_summary/Utils/Utils.Metrics.fs
+++ coronavirus_dashboard_summary/Utils/Utils.Metrics.fs
@@ -4,13 +4,30 @@ open System.Collections.Generic
 open coronavirus_dashboard_summary.Models
 open coronavirus_dashboard_summary.Utils.Constants
 
-type GeneralPayload = Dictionary<string, DB.Payload>
+    
+/// <summary>
+/// Constructs tuples from payload data, structured as (metric name, payload).
+/// </summary>
+/// <param name="item">Data payload</param>
+let inline private constructPayloadTuple (item: DB.Payload): string * DB.Payload =
+    (item.metric, item)
 
-let inline Processor (payload: GeneralPayload) =
-    let inline metric (metric: string) =
-        let inline attr (attribute: string) =
-            match payload.TryGetValue metric with
-            | true, m -> m.getter attribute
-            | _       -> Generic.NotAvailable
-        attr
-    metric
+
+/// <summary>
+/// Creates a dictionary of data from list of tuples.
+/// </summary>
+/// <param name="data">List of tuples as (metric name, payload)</param>
+type GeneralPayload (data: DB.Payload list) =
+    inherit Dictionary<string, DB.Payload>(data
+                                           |> List.map constructPayloadTuple
+                                           |> dict)
+    
+    /// <summary>
+    /// Extracts metric and return a specific attribute from its payload.
+    /// </summary>
+    /// <param name="metric">Metric name</param>
+    /// <param name="attribute">Attribute to extract from metric payload</param>
+    member inline this.GetValue (metric: string) (attribute: string) =
+        match this.TryGetValue metric with
+        | true, m -> m.getter attribute
+        | _       -> Generic.NotAvailable
