99d5abec290a17e7363d4ed425c152d79bea5986
Pouria Hadjibagheri <p.h.bagheri@gmail.com>
2022-02-27 12:31:25 +0000

    Adds new generic filters

--- coronavirus_dashboard_summary/Utils/Utils.Filters.fs
+++ coronavirus_dashboard_summary/Utils/Utils.Filters.fs
@@ -2,6 +2,13 @@ module coronavirus_dashboard_summary.Utils.Filters
 
 open coronavirus_dashboard_summary.Models
 
-let ByPriorityAttribute (group: string * DB.Payload list) =
+
+let inline private groupByPriority (payload: DB.Payload): int = payload.priority
+
+
+let inline GroupByPriorityAttribute (group: string * DB.Payload list) =
     snd group
-    |> Seq.minBy (fun v -> v.priority)
+    |> Seq.minBy groupByPriority
+
+
+let inline GroupByMetric (payload: DB.Payload): string = payload.metric
