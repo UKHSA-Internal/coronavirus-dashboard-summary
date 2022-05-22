95179b7aa7fc9159d409214233a5effba91aefca
Pouria Hadjibagheri <p.h.bagheri@gmail.com>
2022-05-22 12:57:10 +0100

    switches cache label to use England

--- coronavirus_dashboard_summary/Models/Models.HomePage.fs
+++ coronavirus_dashboard_summary/Models/Models.HomePage.fs
@@ -77,7 +77,7 @@ let private fetch (redis: Redis.Client) (date: TimeStamp.Release) (metrics: stri
             |> Async.AwaitTask
             
         return! redis.SetAsync
-                    $"area-{date.isoDate}-UK"
+                    $"area-{date.isoDate}-ENGLAND"
                     result
                     {
                         hours   = Random().Next(3, 12)
@@ -89,7 +89,7 @@ let private fetch (redis: Redis.Client) (date: TimeStamp.Release) (metrics: stri
 let Data (date: TimeStamp.Release) (metrics: string []) (redis: Redis.Client): Async<Payload List> =
     async {
         let dbRes () = fetch redis date metrics
-        let! result  = redis.GetAsync $"area-{date.isoDate}-UK" dbRes
+        let! result  = redis.GetAsync $"area-{date.isoDate}-ENGLAND" dbRes
         
         return match result with
                | Some res -> Json.deserialize<Payload list>(res)
